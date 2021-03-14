f1c7b120fb689e00c260534ff216f12ae08b5ac5
Jean-Marc <jm.kleger@gmail.com>
2021-03-14 21:04:44 +0100

    Navigate through file system

--- resources/js/views/admin/files/index.vue
+++ resources/js/views/admin/files/index.vue
@@ -6,17 +6,38 @@
         directoriesObject: {{ directoriesObject }}<br /><br />
         directoriesArray: {{ directoriesArray }}<br /><br />
         items2: {{ items2 }}<br /><br />
+        rootFolder: {{ rootFolder }}<br /><br />
         directoriesObject:<br />
-        <div v-for="(item, index) in directoriesObject" :key="index"> {{ index }} ---> {{ item }} <br /><br /></div><br /><br />
+        <div v-for="(item, index) in directoriesObject" :key="index">{{ index }} ---> {{ item }} <br /><br /></div>
+        <br /><br />
+        currentFolder: {{ currentFolder }}<br />
         opened: {{ opened }}<br />
         difference: {{ difference }}<br />
         goTo: {{ goTo }}<br />
         <v-row no-gutters>
             <v-col cols="4" style="border: 1px solid orange;">
-                <v-treeview shaped hoverable open-on-click :items="items" item-key="name" @update:open="updateOpen"></v-treeview>
+                <v-treeview shaped hoverable open-on-click return-object :items="items2" item-key="name" @update:open="updateOpen">
+                    <template v-slot:label="{ item }">
+                        <span v-if="item.file" class="file" @click="download(item)">
+                            {{ item.name }}
+                        </span>
+                        <span style="border: 1px dashed green;" v-else>
+                            {{ item.name }}
+                        </span>
+                    </template>
+                </v-treeview>
             </v-col>
             <v-col cols="8" style="border: 1px solid purple;">
-                </v-col>
+                <v-btn small @click="goBack" v-if="currentFolder">&larr; Back</v-btn>
+                <span v-if="currentFolder">Content of {{ currentFolder['name'] }}:</span>
+                <span v-else>Content of root</span>
+                <div class="item" v-if="currentFolder">
+                    <div style="border: 1px solid red; margin: 10px;" v-for="(item, index) in currentFolder.children" :key="index" @click="selectFolder(item)">item: {{ item }}</div>
+                </div>
+                <div class="item" v-else>
+                    <div style="border: 1px dashed red; margin: 10px;" v-for="(item, index) in rootFolder" :key="index" @click="selectFolder(item)">item: {{ item }}</div>
+                </div>
+            </v-col>
         </v-row>
     </div>
 </template>
@@ -47,7 +68,7 @@ export default {
                 this.directoriesArray.push({
                     name: separator[separator.length - 1],
                     id: i + 1,
-                    parentId: this.directoriesObject[separator[separator.length - 2]] ? this.directoriesObject[separator[separator.length - 2]]['id'] : 0,
+                    parentId: this.directoriesObject[separator[separator.length - 2]] ? this.directoriesObject[separator[separator.length - 2]]['id'] : 0
                     // children: []
                 })
             }
@@ -85,7 +106,7 @@ export default {
         }
 
         // Create nested array
-        // this.items2 = this.listToTree(this.directoriesArray)
+        this.items2 = this.listToTree(this.directoriesArray)
 
         // for (let entry in this.directoriesObject) {
         //     console.log('entry: ', entry)
@@ -112,6 +133,10 @@ export default {
             files: [],
             directoriesObject: {},
             directoriesArray: [],
+            currentFolder: null,
+            // currentFolder: {
+            //     children: []
+            // },
             items: [
                 {
                     id: 1,
@@ -119,8 +144,8 @@ export default {
                     children: [
                         { id: 2, name: 'calendar.php' },
                         { id: 3, name: 'chrome.php' },
-                        { id: 4, name: 'webstorm.php' },
-                    ],
+                        { id: 4, name: 'webstorm.php' }
+                    ]
                 },
                 {
                     id: 5,
@@ -135,10 +160,10 @@ export default {
                                     name: 'Src',
                                     children: [
                                         { id: 8, name: 'index.php' },
-                                        { id: 9, name: 'bootstrap.php' },
-                                    ],
-                                },
-                            ],
+                                        { id: 9, name: 'bootstrap.php' }
+                                    ]
+                                }
+                            ]
                         },
                         {
                             id: 10,
@@ -150,12 +175,12 @@ export default {
                                     children: [
                                         { id: 12, name: 'v-btn.php' },
                                         { id: 13, name: 'v-card.php' },
-                                        { id: 14, name: 'v-window.php' },
-                                    ],
-                                },
-                            ],
-                        },
-                    ],
+                                        { id: 14, name: 'v-window.php' }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
                     id: 15,
@@ -163,8 +188,8 @@ export default {
                     children: [
                         { id: 16, name: 'october.pdf' },
                         { id: 17, name: 'november.pdf' },
-                        { id: 18, name: 'tutorial.html' },
-                    ],
+                        { id: 18, name: 'tutorial.html' }
+                    ]
                 },
                 {
                     id: 19,
@@ -176,35 +201,54 @@ export default {
                             children: [
                                 { id: 21, name: 'basic-layouts.mp4' },
                                 { id: 22, name: 'advanced-techniques.mp4' },
-                                { id: 23, name: 'all-about-app.mp4' },
-                            ],
+                                { id: 23, name: 'all-about-app.mp4' }
+                            ]
                         },
                         { id: 24, name: 'intro.pdf' },
-                        { id: 25, name: 'conference-introduction.pdf' },
-                    ],
-                },
+                        { id: 25, name: 'conference-introduction.pdf' }
+                    ]
+                }
             ],
             items2: [],
             active: [],
             opened: [],
             selectedItem: null,
             difference: null,
-            goTo: null,
+            goTo: null
         }
     },
-    computed: {},
+    computed: {
+        rootFolder() {
+            return this.items2.map(a => a.name)
+        }
+    },
     methods: {
         updateOpen(items) {
-            // console.log('items: ', items)
+            console.log('updateOpen items: ', items)
             // console.log('opened: ', this.opened)
+            console.log('abc: ', items[items.length - 1])
             if (this.opened.length > 0) {
-                this.difference = this.opened.filter((x) => !items.includes(x))[0]
+                this.difference = this.opened.filter(x => !items.map(item => item.name).includes(x))[0]
             }
-            this.opened = items
+            this.opened = items.map(item => item.name)
             if (this.difference) {
-                this.goTo = this.difference + ' parent'
+                // this.goTo = this.difference + ' parent'
+                // this.currentFolder = this.directoriesObject[this.difference]
+                const parentId = this.directoriesObject[this.difference]['parentId']
+                console.log('parentId : ', parentId)
+                if (parentId != 0) {
+                    const parentFolder = this.directoriesArray.find(x => x.id == parentId)
+                    this.goTo = parentFolder['name']
+                    this.currentFolder = this.directoriesObject[this.goTo]
+                } else {
+                    this.goTo = null
+                    this.currentFolder = null
+                }
+                console.log('goTo: ', this.goTo)
+                this.difference = null
             } else {
                 this.goTo = this.opened[this.opened.length - 1]
+                this.currentFolder = this.directoriesObject[this.goTo]
             }
         },
         listToTree(list) {
@@ -229,9 +273,35 @@ export default {
             }
             return roots
         },
-    },
+        selectFolder(item) {
+            console.log('selectFolder: ', item)
+            this.currentFolder = this.directoriesObject[item]
+        },
+        goBack() {
+            try {
+                if (this.currentFolder.parentId == 0) {
+                    this.currentFolder = null
+                } else {
+                    const parentFolder = this.directoriesArray.find(x => x.id == this.currentFolder.parentId)
+                    console.log('parentFolder: ', parentFolder)
+                    this.currentFolder = this.directoriesObject[parentFolder['name']]
+                }
+            } catch (error) {
+                console.log('error: ', error)
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
+.item {
+    margin: 10px;
+}
+.item:hover {
+    cursor: pointer;
+}
+.file:hover {
+    color: blue;
+}
 </style>
